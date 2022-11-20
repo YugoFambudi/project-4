71f599391df8bc45c701a32ffadc1ce3f1710f57
YugoFambudi <yugofambudi15@gmail.com>
2022-11-21 00:02:40 +0700

    up

---
+++ app/src/main/java/com/example/emuhamka/android_small___4_activity.java
@@ -0,0 +1,56 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		android_small___4
+	 *	@date 		Sunday 20th of November 2022 09:44:27 AM
+	 *	@title 		Page 1
+	 *	@author 	Yugo
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+package com.example.emuhamka;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class android_small___4_activity extends Activity {
+
+	
+	private View _bg__android_small___4_ek2;
+	private ImageView backk_1;
+	private View rectangle_5;
+	private TextView selamat_datang_e___muhamka;
+	private ImageView logo_1;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.android_small___4);
+
+		
+		_bg__android_small___4_ek2 = (View) findViewById(R.id._bg__android_small___4_ek2);
+		backk_1 = (ImageView) findViewById(R.id.backk_1);
+		rectangle_5 = (View) findViewById(R.id.rectangle_5);
+		selamat_datang_e___muhamka = (TextView) findViewById(R.id.selamat_datang_e___muhamka);
+		logo_1 = (ImageView) findViewById(R.id.logo_1);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
